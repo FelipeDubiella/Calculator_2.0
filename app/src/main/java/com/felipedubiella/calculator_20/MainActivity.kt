32bc33940638fb4d9b4f74c79bd643fc5e8f474c
Felipe Dubiella de Oliveira <Dubiella2@icloud.com>
2024-07-06 00:06:40 -0300

    feat: Layout is ready

---
+++ app/src/main/java/com/felipedubiella/calculator_20/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.felipedubiella.calculator_20
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.felipedubiella.calculator_20.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        with(binding) {
+
+
+        }
+    }
+
+}
+
